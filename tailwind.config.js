d94af66bf1a23b954343c686e4eb5ce1f2ccf872
Ricardo Rodrigues <ricardor662@gmail.com>
2024-09-06 14:48:29 -0300

    define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      white: "#fff",
+      black: "#000",
+      gray: "#1e1e1e",
+      background: {
+        light: "#f8f8f8",
+        dark: "#141414"
+      },
+      primary: "#bb2d2d",
+      secondary: "#ff9b00",
+      mainText: "#1f2a37",
+      secondaryText: "#6b7280"
+    },
     extend: {},
   },
   plugins: [],
