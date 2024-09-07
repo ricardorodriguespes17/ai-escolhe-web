7a88729fd25153609aa3cf0a7f0ca411a1a99d92
Ricardo Rodrigues <ricardor662@gmail.com>
2024-09-07 02:36:07 -0300

    add FavoriteButton in RecipeCard

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,24 +1,31 @@
 import Logo from "../components/Logo"
+import FavoriteButton from "./FavoriteButton"
 
 type RecipeCardProps = {
   title: string
   subTitle: string
   logoURL?: string
   rating: number
+  favorited: boolean
 }
 
-const RecipeCard = ({ title, subTitle, rating }: RecipeCardProps) => {
+const RecipeCard = ({ title, subTitle, rating, favorited }: RecipeCardProps) => {
   return (
     <div className="flex items-center gap-4 h-fit">
       <div className="flex flex-col items-center gap-2 h-full w-fit">
         <Logo mini />
         <p>{`${rating}/5`}</p>
       </div>
-      <div className="flex flex-col gap-2">
+
+      <div className="flex flex-col min-h-full flex-1 gap-2">
         <p>{title}</p>
         <p className="text-secondaryText text-sm">
           {subTitle}
         </p>
+      </div>
+
+      <div className="h-full">
+        <FavoriteButton favorited={favorited} />
       </div>
     </div>
   )
