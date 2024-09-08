68c53155b6cf4090f71ac6e03e476ca8c9c77e68
Ricardo Rodrigues <ricardor662@gmail.com>
2024-09-08 16:13:29 -0300

    create Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import Header from "../components/Header"
+import RecipeContainer from "../components/RecipesContainer"
+import SideContainer from "../components/SideContainer"
+
+const HomePage = () => {
+  
+
+  return (
+    <div className="flex flex-col w-full h-full relative overflow-auto">
+      <Header />
+
+      <main className="flex h-auto flex-1 justify-center gap-6 px-4 pb-6 pt-20 bg-background-light">
+        <SideContainer />
+
+        <div className="flex flex-col gap-6">
+          <RecipeContainer />
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default HomePage
