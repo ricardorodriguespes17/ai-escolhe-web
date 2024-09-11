583fd775d985e07edd79225f43ceb88ec605956a
Ricardo Rodrigues <ricardor662@gmail.com>
2024-09-11 00:13:37 -0300

    refactor profile photo to receive url

--- src/components/ProfilePhoto.tsx
+++ src/components/ProfilePhoto.tsx
@@ -1,14 +1,21 @@
 import { twMerge } from "tailwind-merge"
 
-const ProfilePhoto = () => {
+type ProfilePhotoProps = {
+  url?: string
+}
+
+const ProfilePhoto = ({ url = "" }: ProfilePhotoProps) => {
   const className = twMerge(
+    "overflow-hidden",
     "w-[120px] aspect-square rounded-full bg-white",
     "absolute left-1/2 transform -translate-x-1/2 bottom-0 translate-y-1/2",
     "border-4 border-secondary border-solid"
   )
 
   return (
-    <div className={className} />
+    <div className={className} >
+      <img src={url} className="w-full h-full object-cover" />
+    </div>
   )
 }
 
