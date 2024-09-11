f3a681de2cca13a4fb0a5f20e98a1d5f1d164089
Ricardo Rodrigues <ricardor662@gmail.com>
2024-09-11 12:07:42 -0300

    add insights bar in feed model

--- src/components/FeedModelCard.tsx
+++ src/components/FeedModelCard.tsx
@@ -1,5 +1,8 @@
+import { FaHeart, FaRegHeart, FaShare } from "react-icons/fa"
+import Button from "./Button"
 import Card from "./Card"
 import ProfilePhoto from "./ProfilePhoto"
+import { useState } from "react"
 
 type FeedModelCardProps = {
   children?: React.ReactNode
@@ -9,9 +12,22 @@ type FeedModelCardProps = {
 }
 
 const FeedModelCard = ({ children, userName, time, profileURL }: FeedModelCardProps) => {
+  const [liked, setLiked] = useState(false)
+  const [numberOfLikes, setNumberOfLikes] = useState(3)
+
+  const handleLike = () => {
+    setNumberOfLikes(numberOfLikes + 1)
+    setLiked(true)
+  }
+
+  const handleUnlike = () => {
+    setNumberOfLikes(numberOfLikes - 1)
+    setLiked(false)
+  }
+
   return (
-    <Card>
-      <div className="flex gap-2">
+    <Card className="gap-1">
+      <div className="flex gap-2 mb-4">
         <ProfilePhoto
           url={profileURL}
           size="sm"
@@ -24,6 +40,25 @@ const FeedModelCard = ({ children, userName, time, profileURL }: FeedModelCardPr
 
       <div className="flex flex-col w-full gap-3">
         {children}
+      </div>
+
+      <div className="flex items-center">
+        <Button
+          variant="plain"
+          title="Curtir"
+          className="text-xl gap-2"
+          onClick={liked ? handleUnlike : handleLike}
+        >
+          {liked ? <FaHeart /> : <FaRegHeart />}
+          {numberOfLikes}
+        </Button>
+        <Button
+          variant="plain"
+          className="text-xl"
+          title="Compartilhar"
+        >
+          <FaShare />
+        </Button>
       </div>
     </Card>
   )
