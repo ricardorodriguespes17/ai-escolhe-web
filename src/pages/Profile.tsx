ddbcb7655cc55f6b4f7efe2a4c42fc0ae53cdb9b
Ricardo Rodrigues <ricardor662@gmail.com>
2024-09-10 23:53:55 -0300

    refactor profile page layout

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,37 +1,59 @@
+import Button from "../components/Button"
 import Card from "../components/Card"
+import ProfilePhoto from "../components/ProfilePhoto"
 import RecipeContainer from "../components/RecipesContainer"
 
 const ProfilePage = () => {
   return (
     <div className="flex flex-col items-center gap-3">
-      <div className="w-full bg-secondary h-[180px] rounded-lg relative mb-[70px]">
-        <div className="w-[150px] aspect-square rounded-full bg-gray absolute left-1/2 transform -translate-x-1/2 bottom-0 translate-y-1/2" />
+      <div className="w-full bg-primary h-[180px] rounded-lg relative mb-[50px]">
+        <ProfilePhoto />
       </div>
 
       <h1>Ricardo Rodrigues</h1>
 
       <div className="flex gap-3 w-full">
-        <Card className="gap-2">
-          <h2>Seus dados</h2>
+        <div className="flex-1 flex flex-col gap-2">
+          <Card className="gap-1">
+            <h2>Estatísticas</h2>
+            <label>
+              <strong className="text-primary">14</strong> receitas geradas
+            </label>
+            <label>
+              <strong className="text-primary">5</strong> receitas favoritas
+            </label>
+            <label>
+              <strong className="text-primary">7</strong> amizades
+            </label>
+          </Card>
 
-          <p>28/05/1999</p>
-          <p>ricardo@aiescolhe.com</p>
-          <p>{"(77) 988776655"}</p>
-        </Card>
+          <Card className="gap-1">
+            <h2>Seus dados</h2>
+            <label>Criado em 10/09/2024</label>
 
-        <div className="flex-[2] flex flex-col gap-2">
+            <Button className="mt-2">Editar perfil</Button>
+          </Card>
+
+          <Card className="gap-1">
+            <h2>Preferências</h2>
+            <strong className="text-primary">Tema</strong>
+            <label>light</label>
+            <strong className="text-primary">Salvamento automático</strong>
+            <label>Não</label>
+            <strong className="text-primary">Receber novidades via email</strong>
+            <label>Não</label>
+
+            <Button className="mt-2">
+              Sair
+            </Button>
+          </Card>
+        </div>
+
+        <div className="flex-[3] flex flex-col gap-2">
           <RecipeContainer />
           <RecipeContainer />
           <RecipeContainer />
         </div>
-
-        <Card className="gap-2">
-          <h2>Seus dados</h2>
-
-          <p>28/05/1999</p>
-          <p>ricardo@aiescolhe.com</p>
-          <p>{"(77) 988776655"}</p>
-        </Card>
       </div>
     </div>
   )
