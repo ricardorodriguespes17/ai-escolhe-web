c2e959c01992809f329b568abe17c23338a869e1
Ricardo Rodrigues <ricardor662@gmail.com>
2024-09-11 11:37:40 -0300

    refactor profile photo

--- src/components/ProfilePhoto.tsx
+++ src/components/ProfilePhoto.tsx
@@ -1,20 +1,42 @@
+import { useState } from "react"
+import { BiUser } from "react-icons/bi"
 import { twMerge } from "tailwind-merge"
 
 type ProfilePhotoProps = {
   url?: string
+  size?: "sm" | "md"
+  floating?: boolean
 }
 
-const ProfilePhoto = ({ url = "" }: ProfilePhotoProps) => {
+const ProfilePhoto = ({ url = "", size = "md", floating = true }: ProfilePhotoProps) => {
+  const [error, setError] = useState(false)
+
+  const sizes = {
+    sm: "w-[80px]",
+    md: "w-[120px]"
+  }
+
   const className = twMerge(
     "overflow-hidden",
-    "w-[120px] aspect-square rounded-full bg-white",
-    "absolute left-1/2 transform -translate-x-1/2 bottom-0 translate-y-1/2",
+    sizes[size],
+    "flex items-center justify-center",
+    "text-4xl text-primary",
+    "aspect-square rounded-full bg-white",
+    floating && "absolute left-1/2 transform -translate-x-1/2 bottom-0 translate-y-1/2",
     "border-4 border-secondary border-solid"
   )
 
   return (
     <div className={className} >
-      <img src={url} className="w-full h-full object-cover" />
+      {error ? (
+        <img
+          src={url}
+          className="w-full h-full object-cover"
+          onError={() => setError(true)}
+        />
+      ) : (
+        <BiUser />
+      )}
     </div>
   )
 }
