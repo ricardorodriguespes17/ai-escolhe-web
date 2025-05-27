2a269cfed84a129fbb110e44fe0327fbfa764d18
Ricardo Rodrigues <ricardor662@gmail.com>
2025-05-27 19:12:50 -0300

    refactor: set addRecipe route

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,27 +1,30 @@
-import api from "./api"
-import { AxiosResponse } from "axios"
-import { CreateRecipeType, RecipeType } from "../@types/RecipeTypes"
+import api from "./api";
+import { AxiosResponse } from "axios";
+import { CreateRecipeType, RecipeType } from "../@types/RecipeTypes";
 
 type GenerateRecipeProps = {
-  ingredients: string[]
-}
+  ingredients: string[];
+};
 
 export const generateRecipe = async (data: GenerateRecipeProps) => {
-  return api.post<{}, AxiosResponse<{ recipes: RecipeType[] }>>("/recipe/generate", data)
-}
+  return api.post<{}, AxiosResponse<{ recipes: RecipeType[] }>>(
+    "/recipe/generate",
+    data
+  );
+};
 
 export const getRecipe = async (data: { id: string }) => {
-  return api.get<{}, AxiosResponse<RecipeType>>(`/recipe/${data.id}`)
-}
+  return api.get<{}, AxiosResponse<RecipeType>>(`/recipe/${data.id}`);
+};
 
 export const addRecipe = async (data: CreateRecipeType) => {
-  return api.post<{}, AxiosResponse<RecipeType>>("/recipe", data)
-}
+  return api.post<{}, AxiosResponse<RecipeType>>("/recipe/create", data);
+};
 
 export const updateRecipe = async (data: RecipeType) => {
-  return api.put<{}, AxiosResponse<RecipeType>>("/recipe", data)
-}
+  return api.put<{}, AxiosResponse<RecipeType>>("/recipe", data);
+};
 
 export const deleteRecipe = async (data: { id: string }) => {
-  return api.delete<{}, AxiosResponse<void>>(`/recipe/${data.id}`)
-}
+  return api.delete<{}, AxiosResponse<void>>(`/recipe/${data.id}`);
+};
